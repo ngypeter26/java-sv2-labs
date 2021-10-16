849ea59fa9a1f22d0ecc280285dab78b737b468b
ngypeter26 <ngypeter26@gmail.com>
2021-10-16 23:12:32 +0200

    replace

---
+++ training-solutions/classstructureattributes/src/main/java/clientApp/Client.java
@@ -0,0 +1,7 @@
+package clientApp;
+
+public class Client {
+        String name;
+        int year;
+        String address;
+}
---
+++ training-solutions/classstructureattributes/src/main/java/clientApp/ClientMain.java
@@ -0,0 +1,40 @@
+package clientApp;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        //példányosítás
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+        //beolvasás
+        System.out.println("What is your name?");
+        client.name = scanner.nextLine();
+
+        System.out.println("What is your birth year?");
+      //  option = Integer.parseInt(input.nextLine());
+     //   client.year = scanner.nextInt();
+        client.year = Integer.parseInt(scanner.nextLine());
+
+    //    getClass().getName();
+
+       // String var =	getAttributeTypeName(client.year);
+
+        System.out.println("What is your address?");
+        client.address = scanner.nextLine();
+        //kiírás
+        String newLine = System.getProperty("line.separator");
+
+        int age = 2019 - client.year;
+        System.out.println("Your name is:" + newLine + client.name );
+        System.out.println("Your birth year is:" + newLine + client.year );
+        System.out.println("Your age is:" + newLine +  age );
+        System.out.println("Your address is:" + newLine + client.address );
+
+
+
+
+    }
+
+
+}
