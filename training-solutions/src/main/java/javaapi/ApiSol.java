cfada7c8048d7fb25b5600e5ef9e93e209bbb1eb
ngypeter26 <ngypeter26@gmail.com>
2021-10-20 23:02:30 +0200

    API

---
+++ training-solutions/src/main/java/javaapi/ApiSol.java
@@ -0,0 +1,6 @@
+package javaapi;
+
+public class ApiSol {
+  //  https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html#nextInt--
+   // https://docs.oracle.com/javase/8/docs/api/java/util/Scanner.html#nextLine--
+}
---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,8 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String input = "Hello World!";
+        System.out.println(input.toUpperCase());
+    }
+}
