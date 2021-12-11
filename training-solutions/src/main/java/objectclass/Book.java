c07e32fa2b1dc9e0495b4cb722167011d9b4b2b5
ngypeter26 <ngypeter26@gmail.com>
2021-12-11 17:48:40 +0100

    object osztály

---
+++ training-solutions/src/main/java/objectclass/Book.java
@@ -0,0 +1,11 @@
+package objectclass;
+
+public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+}
